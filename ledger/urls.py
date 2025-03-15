930898521c6b21fa83bd70f90cfec3a709cdc175
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-15 22:09:55 +0800

    [templates, urls.py] Removed the homepage.

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import home
 from .views import recipe_list
 from .views import recipe_details
 
 urlpatterns = [
-	path("", home, name="home"),
 	path("recipes/list", recipe_list, name="recipe_list"),
     path("recipe/<int:number>", recipe_details, name="recipe_details"),
 ]
