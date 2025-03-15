3dd2bf64c01aefc547b1868b843267f5d887428a
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-15 22:40:01 +0800

    [admin.py] Added admin customization.

--- ledger/admin.py
+++ ledger/admin.py
@@ -3,14 +3,36 @@ from .models import Ingredient, Recipe, RecipeIngredient
 
 # Register your models here.
 
+class RecipeIngredientInline(admin.TabularInline):
+	model = RecipeIngredient
+
 class IngredientAdmin(admin.ModelAdmin):
 	model = Ingredient
+
+	search_fields = ('name',)
+
+	list_display = ('name',)
+
+	inlines = [RecipeIngredientInline,]
 
 class RecipeAdmin(admin.ModelAdmin):
 	model = Recipe
 
+	search_fields = ('name',)
+
+	list_display = ('name',)
+
+	inlines = [RecipeIngredientInline,]
+
+
 class RecipeIngredientAdmin(admin.ModelAdmin):
 	model = RecipeIngredient
+
+	search_fields = ('recipe', 'ingredient',)
+
+	list_display = ('quantity', 'ingredient', 'recipe',)
+
+	list_filter = ('recipe',)
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
