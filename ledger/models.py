8632fffe0b1797cb5d16e2131fa1436d6e8f7f81
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-15 20:59:39 +0800

    [models.py] Implemented the Ingredient, Recipe, and RecipeIngredient models.

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# Ingredient - name
+# Recipe - name
+# RecipeIngredient - quantity, ingredient, recipe
+
+class Ingredient(models.Model):
+	name = models.CharField(max_length=255)
+
+class Recipe(models.Model):
+	name = models.CharField(max_length=255)
+
+class RecipeIngredient(models.Model):
+	name = models.CharField(max_length=255)
+	ingredient = models.ForeignKey(Ingredient)
+	recipe = models.ForeignKey(Recipe)
