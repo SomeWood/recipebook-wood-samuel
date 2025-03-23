a9b89db6e88c17a8076ca217406f08ba8930c7af
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-23 19:10:14 +0800

    [templates, views.py] Redirected homepage to login.

--- ledger/views.py
+++ ledger/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Ingredient, Recipe, RecipeIngredient
@@ -19,5 +19,4 @@ def recipe_details(request, param):
     return render(request, 'recipe_details.html', {'recipe':recipe})
 
 def home_page(request):
-
-    return render(request, 'home_page.html')
+    return redirect('/accounts/login')
