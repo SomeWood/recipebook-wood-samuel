607e3f7ec8a031b40c6836304ab4d9f9672a184c
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-23 14:51:36 +0800

    [models.py] Added Author, CreatedOn, and UpdatedOn fields to Recipe model.

--- ledger/models.py
+++ ledger/models.py
@@ -23,6 +23,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
 	name = models.CharField(max_length=255)
+	author = models.CharField(max_length=255)
+	created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return str(self.name)
