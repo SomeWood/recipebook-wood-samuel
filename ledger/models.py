ad138a67ab1f08d11671ef0916ddd6c26fdb8ac6
Samuel Wood <woodsamueldrury@gmail.com>
2025-03-23 14:45:18 +0800

    [models.py] Added Profile model.

--- ledger/models.py
+++ ledger/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 # Ingredient - name
 # Recipe - name
 # RecipeIngredient - quantity, ingredient, recipe
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	bio = models.CharField(max_length=255)
 
 class Ingredient(models.Model):
 	name = models.CharField(max_length=255)
