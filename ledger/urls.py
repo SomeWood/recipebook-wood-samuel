cceaba5048373130fa650475a59f4fc7c437d074
Samuel Wood <woodsamueldrury@gmail.com>
2025-04-17 19:52:27 +0800

    [urls.py, views.py] Made slight corrections to previous commit.

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import recipe_list, recipe_details, home_page, recipe_new
+from .views import recipe_list, recipe_details, home_page, recipe_new, recipe_image
 
 urlpatterns = [
 	path("", home_page, name="home_page"),
 	path("recipes/list", recipe_list, name="recipe_list"),
     path("recipe/<int:param>", recipe_details, name="recipe_details"),
     path("recipe/add", recipe_new, name="recipe_new")
-    path("recipe/pk/add_image", recipe_image, name="recipe_image")
+    path("recipe/<int:pk>/add_image", recipe_image, name="recipe_image")
 ]
 
 app_name = "ledger"
